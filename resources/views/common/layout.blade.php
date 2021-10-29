e8cbe9960d78ce2904cdf00dbf588b5da0c1df88
cyclopicdog <jonathan_doig@hotmail.com>
2021-10-30 01:38:03 +0200

    styled index page, used js to give movies a list ranking and added my own scroll buttons (like this the ranking doesn't reset to 1 every time the list changes). However, because of this the page is slow to load initially. Show page isn't styled.

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Best movies</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>{{ $title }}</title>
 </head>
 <body>
-    
+    <main class="main">
+        @yield('content')
+    </main>
 </body>
 </html>
--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -1,33 +1,37 @@
-@extends('common/layout')
+@extends('common/layout', ['title'=> $movie->name])
 
+@section('content')
+<div>
 <h1>{{$movie->name}}</h1>
-<p>Year: {{$movie->year}}</p>
-<p>Votes: {{$movie->votes_nr}}</p>
-<p>Rating: {{$movie->rating}}</p>
-
-<h3>Already reviewed:</h3>
-<ul>
-@foreach ($movie->reviews as $review)
-    <li>
-        <p>Rate: {{$review->rate}} - Comment: {{$review->comment}}</p>
-    </li>
-@endforeach
-</ul>
-
-
-<h3>Create review:</h3>
-<form action="/movie/{{$movie->id}}" method="post">
-    @csrf
-    <label for="rate">Rate {{$movie->name}}</label>
-    <br>
-    <input type="number" name="rate" id="rate">
-    <br>
-    <br>
-    <label for="comment">Write your review:</label>
-    <br>
-    <textarea name="comment" id="comment" cols="50" rows="10"></textarea>
-    <br>
-    <br>
-    <button>Save review</button>
-
-</form>
+    <p>Year: {{$movie->year}}</p>
+    <p>Votes: {{$movie->votes_nr}}</p>
+    <p>Rating: {{$movie->rating}}</p>
+    
+    <h3>Already reviewed:</h3>
+    <ul>
+    @foreach ($movie->reviews as $review)
+        <li>
+            <p>Rate: {{$review->rate}} - Comment: {{$review->comment}}</p>
+        </li>
+    @endforeach
+    </ul>
+    
+    
+    <h3>Create review:</h3>
+    <form action="/movie/{{$movie->id}}" method="post">
+        @csrf
+        <label for="rate">Rate {{$movie->name}}</label>
+        <br>
+        <input type="number" name="rate" id="rate">
+        <br>
+        <br>
+        <label for="comment">Write your review:</label>
+        <br>
+        <textarea name="comment" id="comment" cols="50" rows="10"></textarea>
+        <br>
+        <br>
+        <button>Save review</button>
+    
+    </form>
+</div>
+@endsection
